dfc60cba8dc51502eec9e9c10a85a9b5dfc99350
Alex Rintt <alexrintt@gmail.com>
2022-11-07 20:40:32 -0300

    (#107) Migrate `/example` to `v0.6.0` and update `pubspec.yaml` version

--- example/lib/screens/file_explorer/file_explorer_card.dart
+++ example/lib/screens/file_explorer/file_explorer_card.dart
@@ -6,7 +6,7 @@ import 'dart:typed_data';
 import 'package:fl_toast/fl_toast.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:shared_storage/saf.dart';
+import 'package:shared_storage/shared_storage.dart';
 
 import '../../theme/spacing.dart';
 import '../../utils/apply_if_not_null.dart';
