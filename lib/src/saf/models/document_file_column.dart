4ef7cd7eab5dfa98bd4dd08664dfd1ffd85a9cfa
Alex Rintt <51419598+alexrintt@users.noreply.github.com>
2023-06-04 03:12:48 -0300

    Refactor [DocumentFileColumn] class with Dart 2.17 enhanced enum feature

--- lib/src/saf/models/document_file_column.dart
+++ lib/src/saf/models/document_file_column.dart
@@ -1,58 +1,33 @@
 /// Representation of the available columns of `DocumentsContract.Document.<Column>`
 ///
 /// [Refer to details](https://developer.android.com/reference/android/provider/DocumentsContract.Document)
-class DocumentFileColumn {
-  const DocumentFileColumn._(this._id);
-
-  final String _id;
-
-  static const _kPrefix = 'DocumentFileColumn';
-
+enum DocumentFileColumn {
   /// Equivalent to [`COLUMN_DOCUMENT_ID`](https://developer.android.com/reference/android/provider/DocumentsContract.Document#COLUMN_DOCUMENT_ID)
-  static const id = DocumentFileColumn._('$_kPrefix.COLUMN_DOCUMENT_ID');
+  id('COLUMN_DOCUMENT_ID'),
 
   /// Equivalent to [`COLUMN_DISPLAY_NAME`](https://developer.android.com/reference/android/provider/DocumentsContract.Document#COLUMN_DISPLAY_NAME)
-  static const displayName =
-      DocumentFileColumn._('$_kPrefix.COLUMN_DISPLAY_NAME');
+  displayName('COLUMN_DISPLAY_NAME'),
 
   /// Equivalent to [`COLUMN_MIME_TYPE`](https://developer.android.com/reference/android/provider/DocumentsContract.Document#COLUMN_MIME_TYPE)
-  static const mimeType = DocumentFileColumn._('$_kPrefix.COLUMN_MIME_TYPE');
+  mimeType('COLUMN_MIME_TYPE'),
 
   /// Equivalent to [`COLUMN_LAST_MODIFIED`](https://developer.android.com/reference/android/provider/DocumentsContract.Document#COLUMN_LAST_MODIFIED)
-  static const lastModified =
-      DocumentFileColumn._('$_kPrefix.COLUMN_LAST_MODIFIED');
+  lastModified('COLUMN_LAST_MODIFIED'),
 
   /// Equivalent to [`COLUMN_SIZE`](https://developer.android.com/reference/android/provider/DocumentsContract.Document#COLUMN_SIZE)
-  static const size = DocumentFileColumn._('$_kPrefix.COLUMN_SIZE');
+  size('COLUMN_SIZE'),
 
   /// Equivalent to [`COLUMN_SUMMARY`](https://developer.android.com/reference/android/provider/DocumentsContract.Document#COLUMN_SUMMARY)
-  static const summary = DocumentFileColumn._('$_kPrefix.COLUMN_SUMMARY');
+  summary('COLUMN_SUMMARY');
 
-  /// Equivalent to [`COLUMN_FLAGS`](https://developer.android.com/reference/android/provider/DocumentsContract.Document#COLUMN_FLAGS)
-  static const flags = DocumentFileColumn._('$_kPrefix.COLUMN_FLAGS');
+  const DocumentFileColumn(this.androidEnumItemName);
 
-  /// Equivalent to [`COLUMN_ICON`](https://developer.android.com/reference/android/provider/DocumentsContract.Document#COLUMN_ICON)
-  static const icon = DocumentFileColumn._('$_kPrefix.COLUMN_FLAGS');
+  static const _kAndroidEnumTypeName = 'DocumentFileColumn';
 
-  @override
-  bool operator ==(Object other) {
-    return other is DocumentFileColumn && other._id == _id;
-  }
+  final String androidEnumItemName;
 
-  static const values = <DocumentFileColumn>[
-    id,
-    displayName,
-    mimeType,
-    lastModified,
-    size,
-    summary,
-    flags,
-    icon,
-  ];
+  String get androidEnumItemId => '$_kAndroidEnumTypeName.$androidEnumItemName';
 
   @override
-  int get hashCode => _id.hashCode;
-
-  @override
-  String toString() => _id;
+  String toString() => androidEnumItemId;
 }
