14b7abaf92a9ba0e4029e1374d99d8e37d84a9fa
Alex Rintt <51419598+alexrintt@users.noreply.github.com>
2023-06-04 04:12:35 -0300

    Remove support for [getRealPathFromUri] closes #55

--- lib/src/saf/api/utility.dart
+++ lib/src/saf/api/utility.dart
@@ -1,4 +1,3 @@
-import '../../channels.dart';
 import '../common/barrel.dart';
 import '../models/barrel.dart';
 
@@ -11,12 +10,3 @@ import '../models/barrel.dart';
 /// {@endtemplate}
 Future<DocumentFile?> fromTreeUri(Uri uri) async =>
     invokeMapMethod('fromTreeUri', <String, String>{'uri': '$uri'});
-
-/// {@template sharedstorage.saf.getDocumentContentAsString}
-/// Helper method to generate the file path of the given `uri`
-///
-/// See [Get real path from URI, Android KitKat new storage access framework](https://stackoverflow.com/questions/20067508/get-real-path-from-uri-android-kitkat-new-storage-access-framework/20559175#20559175)
-/// for details.
-/// {@endtemplate}
-Future<String?> getRealPathFromUri(Uri uri) async => kDocumentFileHelperChannel
-    .invokeMethod('getRealPathFromUri', <String, String>{'uri': '$uri'});
